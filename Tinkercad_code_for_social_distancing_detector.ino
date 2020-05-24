9f0bd433a5132cb913b84c71d358a057979a3dae
Tanmay Agarwal <54680350+Tanmay3610@users.noreply.github.com>
2020-05-24 18:25:43 +0530

    Create Tinkercad_code_for_social_distancing_detector.ino

---
+++ Tinkercad_code_for_social_distancing_detector.ino
@@ -0,0 +1,43 @@
+const int pingPin = 13; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 12; // Echo Pin of Ultrasonic Sensor
+const int buzzer = 11; // Buzzer Pin
+void setup() {
+   Serial.begin(9600); // Starting Serial Terminal
+}
+
+void loop() {
+   
+   long duration, inches, cm;
+   pinMode(pingPin, OUTPUT);
+   digitalWrite(pingPin, LOW);
+   pinMode(buzzer, OUTPUT);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(pingPin, LOW);
+   pinMode(echoPin, INPUT);
+   duration = pulseIn(echoPin, HIGH);
+   inches = microsecondsToInches(duration);
+   if(inches <= 72){
+   	tone(buzzer, (72-inches)*10);
+   }
+   else{
+  	noTone(buzzer);
+   }
+   
+   cm = microsecondsToCentimeters(duration);
+   Serial.print(inches);
+   Serial.print("in, ");
+   Serial.print(cm);
+   Serial.print("cm");
+   Serial.println();
+   delay(100);
+}
+
+long microsecondsToInches(long microseconds) {
+   return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
+}
